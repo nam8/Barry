3cb00cceddac6033b0e58965911d0223098fc2ce
Nina Maksimova <nina.maksimova.15@gmail.com>
2022-02-02 12:14:02 -0500

    Reproducible optimiser vs dynesty test: optimiser gives alpha ~ 1, but dynesty gives alpha ~1.6, for {sdss NGC z0.5 + Ding/Beutler}

--- config/ding_nina_test.py
+++ config/ding_nina_test.py
@@ -5,27 +5,36 @@ from barry.cosmology.camb_generator import getCambGenerator
 from barry.postprocessing import BAOExtractor
 from barry.config import setup
 from barry.utils import weighted_avg_and_std, get_model_comparison_dataframe
-from barry.models import PowerDing2018
-from barry.datasets import PowerSpectrum_SDSS_DR12_Z061_NGC
+from barry.models import PowerDing2018, PowerBeutler2017
+from barry.datasets import PowerSpectrum_SDSS_DR12, PowerSpectrum_AbacusSummit
 from barry.samplers import DynestySampler
 from barry.fitter import Fitter
 import numpy as np
 import pandas as pd
 
-
-# Check if B17 and D18 results change if we apply the BAO extractor technique.
-# Spoiler: They do not.
 if __name__ == "__main__":
     pfn, dir_name, file = setup(__file__)
     fitter = Fitter(dir_name, remove_output=False)
 
     sampler = DynestySampler(temp_dir=dir_name, nlive=200)
 
-    d = PowerSpectrum_SDSS_DR12_Z061_NGC(recon=True, realisation=0)
-    ding = PowerDing2018(recon=True)
-    
+    # redshift_bin = 2 --> z = 0.51. Default is redshift_bin = 3 (z = 0.61). 
+    d = PowerSpectrum_SDSS_DR12(recon="iso", realisation=0, redshift_bin=2) 
 
-    d.set_realisation(0)
+    ding = PowerDing2018(recon=d_as.recon)
+    beutler = PowerBeutler2017(recon=d_as.recon)
+    
+    # this returns alpha ~ 1
+    ding.sanity_check(d)
+    # and so do this 
+    beutler.sanity_check(d)
+
+    # but dynesty [below] will give:
+    # {'D18':    realisation    avg       std       max     posterior
+    #    0         0          1.641669  0.277169  1.279457 -14.720054
+    #    1         0          1.643549  0.271477  1.374330 -14.823846}
+
+    fitter.add_model_and_dataset(beutler, d, name=f"D18", linestyle="-", color="p", realisation=0)
     fitter.add_model_and_dataset(ding, d, name=f"D18", linestyle="-", color="p", realisation=0)
            
     fitter.set_sampler(sampler)
@@ -33,6 +42,5 @@ if __name__ == "__main__":
     fitter.fit(file)
 
     model_results, summary = get_model_comparison_dataframe(fitter)
-
     print(model_results)
 
